4a10d4e9089364f408e90c4576f22c6af79dafb9
snabbat <nabbat.soufiane@gmail.com>
2025-09-15 16:38:18 +0100

    Update saham_edge.py

--- airflow/maestro/core/saham_edge.py
+++ airflow/maestro/core/saham_edge.py
@@ -1,20 +1,4 @@
-from airflow import DAG
-from airflow.operators.bash import BashOperator
-from datetime import datetime
-
-# DAG simple pour DCO à 18h
-dag = DAG(
-    dag_id='daily_dco_simple',
-    description='Lance dco.sh chaque jour à 18h',
-    schedule_interval='0 18 * * *',  # 18h00 tous les jours
-    start_date=datetime(2025, 1, 1),
-    catchup=False,
-    tags=['dco', 'daily']
-)
-
-# Tâche unique - Exécuter dco.sh
-run_dco = BashOperator(
-    task_id='run_dco_script',
-    bash_command='bash /opt/airflow/apps/dco.sh',
-    dag=dag
-)
+ADD https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/3.3.4/hadoop-aws-3.3.4.jar /opt/spark/jars/
+ADD https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/1.11.1026/aws-java-sdk-bundle-1.11.1026.jar /opt/spark/jars/
+ADD https://repo1.maven.org/maven2/org/apache/iceberg/iceberg-spark-runtime-3.5_2.12/1.7.2/iceberg-spark-runtime-3.5_2.12-1.7.2.jar /opt/spark/jars/
+ADD https://repo1.maven.org/maven2/org/apache/iceberg/iceberg-hive-runtime/1.7.2/iceberg-hive-runtime-1.7.2.jar /opt/spark/jars/
